149a519bb9ee6f4d90acf8d307d00b8f109bf098
NguyenThanhSon2002 <nguyenthanhsonyb2002@gmail.com>
2025-11-18 19:50:41 +0700

    Update 18112025

--- Testing_System_Assignment_1.sql
+++ Testing_System_Assignment_1.sql
@@ -11,12 +11,32 @@ CREATE TABLE Department (
     DepartmentName VARCHAR(50) NOT NULL UNIQUE KEY
 );
 
+INSERT INTO Department(DepartmentID, DepartmentName) 
+VALUES				  
+						(1, 'Marketing'),
+                        (2, 'Sale'),
+                        (3, 'Bảo vệ'),
+                        (4, 'Nhân sự'),
+                        (5, 'Kỹ thuật'),
+                        (6, 'Tài chính'),
+                        (7, 'Phó giám đốc'),
+                        (8, 'Giám đốc'),
+                        (9, 'Thư kí'),
+                        (10, 'Bán hàng');
+                        
 -- Table 2: Position
 DROP TABLE IF EXISTS `Position`;
 CREATE TABLE `Position` (
     PositionID TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     PositionName ENUM('Dev', 'Test', 'Scrum Master', 'PM') NOT NULL
 );
+
+INSERT INTO `Position` (PositionName) 
+VALUES				  
+						('Dev'),
+                        ('Test'),
+                        ('Scrum Master'),
+                        ('PM');
 
 -- Table 3: Account 
 DROP TABLE IF EXISTS `Account`;
@@ -32,6 +52,19 @@ CREATE TABLE `Account` (
     CreateDate DATETIME NOT NULL DEFAULT NOW()
 );
 
+INSERT INTO `Account` (Email,Username,FullName,DepartmentID,PositionID) 
+VALUES				  
+					  ('nguyenvana01@gmail.com','NguyenVanA01','Nguyen Van A',1,1),
+					  ('nguyenvanb02@gmail.com','NguyenVanB02','Nguyen Van B',2,2),
+					  ('nguyenvanc03@gmail.com','NguyenVanC03','Nguyen Van C',3,3),
+					  ('nguyenvand04@gmail.com','NguyenVanD04','Nguyen Van D',4,4),
+                      ('nguyenvane05@gmail.com','NguyenVanE05','Nguyen Van E',5,4),
+                      ('nguyenvanf06@gmail.com','NguyenVanF06','Nguyen Van F',6,3),
+                      ('nguyenvang07@gmail.com','NguyenVanG07','Nguyen Van G',7,2),
+                      ('nguyenvanh08@gmail.com','NguyenVanH08','Nguyen Van H',8,1),
+                      ('nguyenvani09@gmail.com','NguyenVanI09','Nguyen Van I',9,1),
+                      ('nguyenvank010@gmail.com','NguyenVanK10','Nguyen Van K',10,2);
+
 -- Table 4: Group  
 DROP TABLE IF EXISTS `Group`;
 CREATE TABLE `Group` (
@@ -41,14 +74,42 @@ CREATE TABLE `Group` (
     CreateDate DATETIME NOT NULL DEFAULT NOW()
 );
 
+INSERT INTO `Group`(GroupName, CreatorID) 
+VALUES				  
+						('MarketingDepartment', 1),
+                        ('SaleDepartment', 2),
+                        ('SecurityDepartment', 3),
+                        ('HumanResourcesDepartment', 4),
+                        ('ITDepartment', 5),
+                        ('FinanceDepartment', 6),
+                        ('DeputyDirectorDepartment', 7),
+                        ('DirectorDepartment', 8),
+                        ('SecretarygDepartment', 9),
+                        ('SellDepartment', 10);
+
 -- Table 5: GroupAccount
 DROP TABLE IF EXISTS GroupAccount;
 CREATE TABLE GroupAccount (
-    GroupID TINYINT UNSIGNED PRIMARY KEY,
+    GroupID TINYINT UNSIGNED,
     FOREIGN KEY (GroupID) REFERENCES `Group`(GroupID),
-    AccountID TINYINT UNSIGNED UNIQUE KEY,
+    AccountID TINYINT UNSIGNED,
+    FOREIGN KEY (AccountID) REFERENCES `Account`(AccountID),
+    PRIMARY KEY (GroupID, AccountID),
     JoinDate DATETIME NOT NULL DEFAULT NOW()
 );
+
+INSERT INTO GroupAccount(GroupID, AccountID) 
+VALUES				  
+						(1, 110),
+                        (2, 111),
+                        (3, 112),
+                        (4, 113),
+                        (5, 114),
+                        (6, 115),
+                        (7, 116),
+                        (8, 117),
+                        (9, 118),
+                        (10, 119);
 
 -- Table 6: TypeQuestion
 DROP TABLE IF EXISTS TypeQuestion;  
@@ -57,12 +118,26 @@ CREATE TABLE TypeQuestion (
     TypeName ENUM('Essay', 'Multiple-Choice') NOT NULL
 );
 
+INSERT INTO TypeQuestion(TypeID, TypeName) 
+VALUES				  
+						(1, 'Essay'),
+                        (2, 'Multiple-Choice');
+
 -- Table 7: CategoryQuestion 
 DROP TABLE IF EXISTS CategoryQuestion; 
 CREATE TABLE CategoryQuestion (
     CategoryID TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     CategoryName VARCHAR(10) NOT NULL
 );
+
+INSERT INTO CategoryQuestion(CategoryName) 
+VALUES				  
+						('Kinh nghiệm'),
+                        ('Chuyên môn'),
+                        ('Các kỹ năng liên quan'),
+                        ('Học vấn'),
+                        ('Thông tin cơ bản'),
+                        ('Các kỹ năng khác');
 
 -- Table 8: Question 
 DROP TABLE IF EXISTS Question; 
@@ -72,6 +147,7 @@ CREATE TABLE Question (
     CategoryID TINYINT UNSIGNED NOT NULL,
     FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID),
     TypeID TINYINT UNSIGNED NOT NULL UNIQUE KEY,
+    FOREIGN KEY (TypeID) REFERENCES TypeQuestion (TypeID),
     CreatorID TINYINT UNSIGNED NOT NULL UNIQUE KEY,
     CreateDate DATETIME NOT NULL DEFAULT NOW()
 );
